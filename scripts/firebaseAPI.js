8f9c1d014f604716858641024167a2b5edab556a
Kai Oh <kaka8651@gmail.com>
2021-11-02 18:56:33 -0700

    Made firebaseAPI for Book it

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBrHYgVBtlG1z7cyEjZIv8aPiiWIgWifo4",
+  authDomain: "book-it-5ab7c.firebaseapp.com",
+  projectId: "book-it-5ab7c",
+  storageBucket: "book-it-5ab7c.appspot.com",
+  messagingSenderId: "320719821616",
+  appId: "1:320719821616:web:e34f7b28198b12082e86f5",
+  measurementId: "G-4ZD4YVV5F1"
+};
+  
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
