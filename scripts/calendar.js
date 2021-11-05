11e3f108e311336647e1a3e9e3e9e2fa5eeab9ec
RealGoldenGeneral <ncheng25@my.bcit.ca>
2021-11-05 11:48:43 -0700

    Added content

---
+++ scripts/calendar.js
@@ -0,0 +1,288 @@
+function dates() {
+    var datesRef = db.collection("Dates");
+
+    datesRef.add({
+        month:"November",
+        date:"05",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"06",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"07",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"08",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"09",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"10",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"11",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"12",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"13",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"14",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"15",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"16",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"17",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"18",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"19",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"20",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"21",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"22",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"23",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"24",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"25",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"26",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"27",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"28",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"29",
+    })
+
+    datesRef.add({
+        month:"November",
+        date:"30",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"01",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"02",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"03",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"04",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"05",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"06",
+    })
+    
+    datesRef.add({
+        month:"December",
+        date:"07",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"08",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"09",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"10",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"11",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"12",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"13",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"14",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"15",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"16",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"17",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"18",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"19",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"20",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"21",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"22",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"23",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"24",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"25",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"26",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"27",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"28",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"29",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"30",
+    })
+
+    datesRef.add({
+        month:"December",
+        date:"31",
+    })
+}
