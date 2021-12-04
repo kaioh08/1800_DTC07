77f94a0c7b05ee86171d73eb87c99e93787de7fd
RealGoldenGeneral <ncheng25@my.bcit.ca>
2021-12-03 17:48:39 -0800

    cleaned various js of unused code

--- scripts/boxing.js
+++ scripts/boxing.js
@@ -35,29 +35,6 @@ function addBoxMon() {
     });
 }
 
-// document.getElementById("book-mon-box").onclick = function () {
-//     var tempRef = db.collection("temp_data");
-//     tempRef.doc("test").set({
-//             code: "box1",
-//             name: "Boxing",
-//             city: "Vancouver",
-//             date: "Monday, Dec 6",
-//             spots: "28/30",
-//             location: "Sugarray's Boxing Gym",
-//             age: "All Ages",
-//             time: "6:30am - 8:30am",
-//             price: "5$",
-//         })
-//         .then(() => {
-//             location.href = "confirmation.html";
-//             console.log("does it write?");
-//         })
-//         .catch((error) => {
-//             console.error("Error writing document: ", error);
-//         });
-
-// };
-
 function writeboxSpots() {
     //define a variable for the collection you want to create in Firestore to populate data
     var boxingRef = db.collection("Boxing");
@@ -144,25 +121,6 @@ function writeboxSpots() {
         price: "5$",
     });
 
-    // badmintonRef.add({
-    //     code: "bad6",
-    //     name: "",
-    //     code2: "wednesday2",
-    //     code3: "wednesday3",
-    //     code4: "wednesday4",
-    //     code5: "wednesday5",
-    //     code6: "wednesday6",
-    //     code7: "wednesday7",
-    //     city: "Burnaby",
-    //     date: "Wednesday, Dec 11",
-    //     spots: "10/30",
-    //     location: "Christine Sinclair Community Center",
-    //     age: "All Ages",
-    //     duration: "2 hours",
-    //     price: "5$",
-
-    // });
-
 }
 
 function displayBoxing() {
@@ -170,11 +128,7 @@ function displayBoxing() {
         .then(allSpots => {
             allSpots.forEach(doc => {
                 var sportName = doc.data().name; //gets the name field
-                var sportID = doc.data().code;
-                //gets the unique ID field
-                //hange the badminton ode to class
-                // var boxDate = doc.data().date // gest date
-                // var boxSpot = doc.data().spots
+                var sportID = doc.data().code; //gets the unique ID field
                 var boxID3 = doc.data().code3;
                 var boxID4 = doc.data().code4;
                 var boxID5 = doc.data().code5;
@@ -202,9 +156,6 @@ function displayBoxing() {
         })
 }
 
-
-// writeBoxSpots();
-// myFunction();
 displayBoxing();
 
 var options = {
@@ -223,39 +174,10 @@ const th = new Date();
 th.setDate(th.getDate() + ((7 - th.getDay()) % 7 + 4) % 10);
 const f = new Date();
 f.setDate(f.getDate() + ((7 - f.getDay()) % 7 + 5) % 11);
-// function getMonday(n) {
-//     n = new Date(n);
-//     var day = n.getDay(),
-//         diff = n.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
-//     return new Date(n.setDate(diff));
-// }
+
 document.getElementById("testdate").innerText = m.toLocaleDateString("en-US", options);
 document.getElementById("nextmonday").innerText = m.toLocaleDateString("en-US", options);
 document.getElementById("nexttuesday").innerText = t.toLocaleDateString("en-US", options);
 document.getElementById("nextwednesday").innerText = w.toLocaleDateString("en-US", options);
 document.getElementById("nextthursday").innerText = th.toLocaleDateString("en-US", options);
 document.getElementById("nextfriday").innerText = f.toLocaleDateString("en-US", options);
-
-// function writeBadSpots2() {
-//     //define a variable for the collection you want to create in Firestore to populate data
-//     var badmintonsRef = db.collection("Badminton2");
-//     badmintonsRef.add({
-//         code: "bad12",
-//     })
-// }
-// const d = new Date();
-// d.setDate(d.getDate() + ((7 - d.getDay()) % 7 + 1) % 7);
-// getElementById("keke").innerText = d;
-
-// function writeDate() {
-//     // const d2 = new Date();
-//     // d2.setDate(d2.getDate() + ((7 - d2.getDay()) % 7 + 1) % 7);
-//     var dateRef = db.collection("datest")
-//     dateRef.add({
-//         code = "date1",
-//         date = d,
-//         date2 = d2,
-//     })
-// }
-// writeDate();
-// writeBadSpots2();
